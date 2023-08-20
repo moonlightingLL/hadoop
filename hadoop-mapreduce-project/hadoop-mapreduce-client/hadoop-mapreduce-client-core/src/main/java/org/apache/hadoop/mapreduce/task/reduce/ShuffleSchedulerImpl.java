2aebc9bf07e465a773d264d600669ecc013a7b4f
moonlightingLL <nancyliuu@163.com>
2023-08-20 23:35:11 +0800

    adding 0 check

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/task/reduce/ShuffleSchedulerImpl.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/task/reduce/ShuffleSchedulerImpl.java
@@ -344,6 +344,7 @@ public class ShuffleSchedulerImpl<K,V> implements ShuffleScheduler<K,V> {
   private void checkAndInformMRAppMaster(
       int failures, TaskAttemptID mapId, boolean readError,
       boolean connectExcpt, boolean hostFailed) {
+  
         if (maxFetchFailuresBeforeReporting == 0) {
           throw new IllegalArgumentException("maxFetchFailuresBeforeReporting cannot be zero");
         }
